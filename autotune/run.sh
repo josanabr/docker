3e70c6464fb1494301a940b1da0b8bc7590003c7
John Sanabria <john.sanabria@gmail.com>
2017-10-13 15:58:35 -0500

    adding comments to run.sh

--- autotune/run.sh
+++ autotune/run.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
+#
+# Este script se puede ejecutar de las siguientes maneras
+#
+# 1. ./run.sh
+#    Ejecutara el contenedor "josanabr/autotune" y archivo "example_runs.json"
+#
+# 2. ./run.sh micontenedor
+#    Ejecutara el contenedor "micontenedor" y archivo "example_runs.json"
+#
+# 3. ./run.sh micontenedor miarchivo.json
+#    Ejecutara el contenedor "micontenedor" y archivo "miarchivo.json"
+#
+# +----------+
+# |IMPORTANTE| el archivo .json debe existir en el directorio donde se ejecuta
+# +----------+ el script 
+#
+# Autor: John Sanabria - john.sanabria@correounivalle.edu.co
+# Fecha: 2017-10-13
+#
 if [ "x" == "x${1}" ]; then
 	CONTAINER="josanabr/autotune"
 	JSONFILE="example_runs.json"
