da78d1754654d10f41cf54567bc56324186e4576
John Sanabria <john.sanabria@gmail.com>
2017-10-13 15:53:17 -0500

    Fixing run.sh

--- autotune/run.sh
+++ autotune/run.sh
@@ -10,5 +10,10 @@ else
 		JSONFILE="${2}"
 	fi
 fi
-echo "Ejecutando contenedor ${CONTAINER} con archvio ${JSONFILE}"
-docker run -e jsonfile="/run/${JSONFIFLE}" -v $(pwd):/run ${CONTAINER}
+if [ ! -f "${JSONFILE}" ]; then
+	echo "No se encontro archivo \"${JSONFILE}\""
+	echo "Modo de ejecucion ${0} <contenedor> <archivo.json>"
+	exit -1
+fi
+echo "Ejecutando contenedor \"${CONTAINER}\" con archivo \"${JSONFILE}\""
+time docker run --rm -e jsonfile="/run/${JSONFILE}" -v $(pwd):/run ${CONTAINER}
